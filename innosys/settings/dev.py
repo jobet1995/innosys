a64b90718ca981d806360f0d3e6feac96e7f14ce
jobet1995 <jobetcasquejo221@gmail.com>
2025-11-04 20:38:19 +0800

    Initialize

---
+++ innosys/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-=yoanhwrq23az$tiqdqzix1np_cx(ax15a(=d#*_zvu9k8(73m"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
